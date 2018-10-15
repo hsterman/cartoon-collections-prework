0fc7c7b650c71158652b312a6a9660049d48a718
heather sterman <heather.sterman531@gmail.com>
2018-10-15 21:00:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet# code an argument here
   planeteer_calls = "earth", "wind", "fire", "water", "heart"]
-  planeteer_calls.map!
+  planeteer_calls.map! {|planeteer_calls| planeteer_calls.capitalize }
 end
 
 def long_planeteer_calls# code an argument here
